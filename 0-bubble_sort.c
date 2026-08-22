a61ca193df01eeb95c580d00320ebbc42d3f49a9
Felix Harding <j.felixharding@gmail.com>
2026-08-22 16:38:22 +1000

    first task

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+*bubble_sort - sorts and array of ints using bubble sort algo
+*@array: the array of ints to sort
+*@size: size of the array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	int swap, temp;
+	size_t counter;
+
+	if (array == NULL)
+		return (-1);
+	swap = 1;
+	while (swap == 1)
+	{
+		swap = 0;
+		counter = 0;
+		while ((counter + 1) < size)
+		{
+			if (array[counter] > array[counter + 1])
+			{
+				temp = array[counter + 1];
+				array[counter + 1] = array[counter];
+				array[counter] = temp;
+				swap = 1;
+				print_array(array, size);
+			}
+			counter++;
+		}
+	}
+}
