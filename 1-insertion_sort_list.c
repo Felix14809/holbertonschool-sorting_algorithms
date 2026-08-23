e5cb38bb737f8ef0c4114cfb39ee2680578b8980
Felix Harding <j.felixharding@gmail.com>
2026-08-23 16:12:56 +1000

    insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+/**
+*bubble_sort - sorts and array of ints using bubble sort algo
+*@array: the array of ints to sort
+*@size: size of the array
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *min, *temp, *index;
+	int sorted = 1;
+	if (*list == NULL)
+		return;
+	index = *list;
+	while (sorted == 1)
+	{
+		sorted = 0;
+		while (index->next != NULL)
+		{
+			temp = index;
+			index = index->next;
+			min = index;
+			while (min->n < temp->n && min->prev != NULL)
+			{
+				if (min->next != NULL)
+					min->next->prev = temp;
+				temp->next = min->next;
+				min->next = temp;
+				min->prev = temp->prev;
+				if (min->prev == NULL)
+				{
+					*list = min;
+					temp->prev = min;
+				}
+				else
+				{
+					temp->prev->next = min;
+					temp->prev = min;
+					temp = min->prev;
+				}
+				print_list(*list);
+				sorted = 1;
+			}
+		}
+	}
+}
